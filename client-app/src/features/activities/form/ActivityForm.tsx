df26a160992b3125a280fcdd6745a32bb4cfc0e9
webmatters <andrew.smith@webmatters.io>
2022-06-12 20:20:14 -0400

    Adds Activity CRUD in client

---
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -0,0 +1,89 @@
+import React, { ChangeEvent, useState } from 'react';
+import { Button, Form, Segment } from 'semantic-ui-react';
+
+import { Activity } from 'app/models/activity';
+
+interface Props {
+  activity: Activity | undefined;
+  closeForm: () => void;
+  createOrEdit: (activity: Activity) => void;
+}
+
+export default function ActivityForm({
+  activity: selectedActivity,
+  closeForm,
+  createOrEdit,
+}: Props) {
+  const initialState = selectedActivity ?? {
+    id: '',
+    title: '',
+    category: '',
+    description: '',
+    date: '',
+    city: '',
+    venue: '',
+  };
+
+  const [activity, setActivity] = useState(initialState);
+
+  function handleSubmit() {
+    createOrEdit(activity);
+  }
+
+  function handleInputChange(
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
+    const { name, value } = event.target;
+    setActivity({ ...activity, [name]: value });
+  }
+
+  return (
+    <Segment clearing>
+      <Form onSubmit={handleSubmit} autoComplete="off">
+        <Form.Input
+          placeholder="title"
+          value={activity.title}
+          name="title"
+          onChange={handleInputChange}
+        />
+        <Form.TextArea
+          placeholder="description"
+          value={activity.description}
+          name="description"
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          placeholder="category"
+          value={activity.category}
+          name="category"
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          placeholder="date"
+          value={activity.date}
+          name="date"
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          placeholder="city"
+          value={activity.city}
+          name="city"
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          placeholder="venue"
+          value={activity.venue}
+          name="venue"
+          onChange={handleInputChange}
+        />
+        <Button floated="right" positive type="submit" content="Submit" />
+        <Button
+          onClick={closeForm}
+          floated="right"
+          type="button"
+          content="Cancel"
+        />
+      </Form>
+    </Segment>
+  );
+}
